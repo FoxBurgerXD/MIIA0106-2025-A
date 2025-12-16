f1cd80d0d5592433d9c5428d6ef06ee8af36eeab
FoxBurgerXD <rattapum1801285@gmail.com>
2025-12-16 17:08:43 +0700

    LAB5

---
+++ lab5/LAB5/LAB5/LAB5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+double rArea(double leight, double width)
+{
+    double area = leight * width;
+    return area;
+}
+
+double cArea(double r)
+{
+    const double PI = 3.14159;
+    double carea = PI * r * r;
+    return carea;
+
+}
+int main()
+{
+    double r, leight, width;
+    cout << "=======Rectangle Area======= " << endl;
+    cout << "Enter Length: ";
+    cin >> leight;
+    cout << "Enter Width: ";
+    cin >> width;
+    cout << "Area of Rectangle: " << rArea(leight, width) << endl;
+
+
+    cout << "=======Circle Area======= " << endl;
+    cout << "Enter Radius: ";
+    cin >> r;
+    cout << "Area of Circle: " << cArea(r) << endl;
+    return 0;
+}
